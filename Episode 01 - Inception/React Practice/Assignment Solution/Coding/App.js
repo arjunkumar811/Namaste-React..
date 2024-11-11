69f523fba0c7c7f342860d1aafc325bd06dd2b93
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-11 19:56:51 +0530

    Added Code Solution

---
+++ Episode 01 - Inception/React Practice/Assignment Solution/Coding/App.js
@@ -0,0 +1,20 @@
+// Q1): Build your `first Hello World` program using, HTML
+/*
+<h1>Hello World using HTML</h1>
+*/
+
+// Q2): Build your `first Hello World` program using, JavaScript DOM
+/*
+const heading = document.createElement("h1");
+heading.textContent = "Hello World using JavaScript DOM";
+
+const root = document.getElementById("root");
+root.appendChild(heading);
+*/
+
+
+// Q3): Build your `first Hello World` program using, React
+const heading = React.createElement("h1", null, "Hello World using React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);

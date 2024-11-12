c99d2866d0f19ba9f282254e4232827ec3ec49a4
arjunkumar811 <kumararjun26401@gmail.com>
2024-11-13 02:42:00 +0530

    Added Solution

--- Episode 04 - Talk is Cheap, Show Me the Code/Code/App.js
+++ Episode 04 - Talk is Cheap, Show Me the Code/Code/App.js
@@ -1,121 +1,143 @@
 import React from "react";
-import ReactDOM  from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import logo from "./images1/mealhub.logo.webp";
-import card1 from "./images1/meghana.png"
 
 /**
  * Header
- * -Logo
- * -Nav items
+ * - Logo
+ * - Nav items
  * Body
- * -Search
- * -RestroContainer
- *   -RestroCards 
- *     -Img
- *     -Name of res
- *     -rating 
- *     -cuisine
- *     -delivary time
+ * - Search
+ * - RestroContainer
+ *   - RestroCards
+ *     - Img
+ *     - Name of res
+ *     - Rating
+ *     - Cuisine
+ *     - Delivery time
  * Footer
- * -Copyright
- * -Links
- * -Address
- * -Contact
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
  */
-//////////////HEADER//////////////////////
+
+////////////// HEADER ///////////////
 
 const Header = () => {
-    return (
-<div className="header" >
-    
-<div className="logo-container"> <img className="logo" src={logo} alt="logo" />  </div>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
 
-<div className="nav-items">
-<ul>
-<li>Home</li>
-<li>About Us</li>
-<li>Contact</li>
-<li>Card</li>
-</ul>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Card</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-</div>
-
-</div>
-);
-};   
-
-
-///////////BODY////////////////////////                                      
+////////////// BODY ///////////////
 
 const Body = () => {
-    console.log(resObject);
-    
-    return (
+  
+
+  return (
     <div className="body">
-        <div className="search">
-            Search
-        </div>
-
-        <div className="res-container">
-        <Rescards  resData = {resObject}  />
-
-    
-        </div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+        <Rescards resData={resObject} />
+      </div>
     </div>
-    );
+  );
 };
 
 const Rescards = (props) => {
-    const { resData } = props;
-    return (
-      
+  const { resData } = props;
+   const { info } = resData; // Extract the `info` object
+
+   const
+   {name,
+    cloudinaryImageId,
+    cuisines,
+    avgRating,
+     deliveryTime,
+      areaName} = resData?.info;
+
+
+  return (
     <div className="res-Cards">
-    
-    <img className="card1" src={card1} alt="card1" /> 
-    <h3>{resData.info.name}</h3>
-    <h4>{resData.info.cuisines}</h4>
-    <h4>{resData.info.avgRating}</h4>
-    <h4>{resData.info.deliveryTime}</h4>
-    <h4>{resData.info.areaName}</h4>
-
-    </div>
-    );
-};
-
-
-const resObject = 
-    {
-        info: {
-          id: "10894",
-          name: "Pizza Hut",
-          cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
-          locality: "2nd Stage",
-          areaName: "BTM Layout",
-          costForTwo: "₹350 for two",
-          cuisines: ["Pizzas"],
-          avgRating: 4.1,
-          parentId: "721",
-          avgRatingString: "4.1",
-          totalRatingsString: "10K+",
-          deliveryTime: 31,
+      <img
+        className="card1"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+         cloudinaryImageId
         }
-    };
-    
-   
-  
-/////////////FOOTER///////////////
-
-const App = () => { 
-return (
-<div className="app">
-< Header />
-<Body />
-
-</div>
-);
+        alt="Restaurant"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4> {/* Display cuisines as comma-separated values */}
+      <h4>Rating: {avgRating}</h4>
+      <h4>Delivery Time: {deliveryTime} mins</h4>
+      <h4>Area: {areaName}</h4>
+    </div>
+  );
 };
 
+////////////// RES OBJECT ///////////////
 
+const resObject = {
+  info: {
+    id: "10894",
+    name: "Pizza Hut",
+    cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    locality: "2nd Stage",
+    areaName: "BTM Layout",
+    costForTwo: "₹350 for two",
+    cuisines: ["Pizzas"],
+    avgRating: 4.1,
+    parentId: "721",
+    avgRatingString: "4.1",
+    totalRatingsString: "10K+",
+    deliveryTime: 31,
+  },
+};
+
+////////////// FOOTER ///////////////
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/ >);
+root.render(<App />);

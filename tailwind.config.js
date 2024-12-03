1c35daaa7a9e23f777c3e0400e7f54ab98de708c
arjunkumar811 <kumararjun26401@gmail.com>
2024-12-03 19:47:23 +0530

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
     "./Episode 10 - Jo Dikhta Hai, Vo Bikta Hai/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
